c734d9f0f329e11460739d30adb4480f7cba3645
Ron Gabriel Ramos <rongabriel01.ramos@gmail.com>
2026-01-06 00:55:02 +0800

    Prevent scaling/zoom

--- pages/income&expenses/addIncome.php
+++ pages/income&expenses/addIncome.php
@@ -24,7 +24,7 @@ $currencySymbol = ($currencyCode === 'USD') ? '$' : '₱';
 
 <head>
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>CtrlSave | Add Income</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../../assets/css/sideBar.css">
--- pages/income&expenses/edit.php
+++ pages/income&expenses/edit.php
@@ -29,7 +29,7 @@ $userCategoriesResult = executeQuery($getuserCategoriesQuery);
 
 <head>
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>CtrlSave | Edit</title>
     <!-- Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
